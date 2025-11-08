44281d94b9f462a049f185a4aec7c2d8214c7e8d
kjos-i <kjos007@gmail.com>
2025-11-08 11:54:52 +0100

    Update UNet_PyTorch_model.py

--- UNet_PyTorch_model.py
+++ UNet_PyTorch_model.py
@@ -2,90 +2,76 @@ import torch
 import torch.nn as nn
 
 """
-This version of the UNet Model is by Sovit Ranjan Rath, copied from:
+U-Net: Convolutional Networks for Biomedical Image Segmentation
+Olaf Ronneberger, Philipp Fischer, Thomas Brox
+Submitted on 18 May 2015, arXiv:1505.04597
+Original image size used: 572x572
+
+This is a modified version of the UNet Model by Sovit Ranjan Rath, copied from:
 https://debuggercafe.com/unet-from-scratch-using-pytorch/
 
-Implemented with padding and BatchNorm2d
+Here implemented with padding (no cropping) and BatchNorm2d
 """
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv_op = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-    
-    def forward(self, x):
-        return self.conv_op(x)
+def double_convolution(in_channels, out_channels):
+    double_conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                                nn.BatchNorm2d(out_channels),
+                                nn.ReLU(inplace=True),
+                                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                                nn.BatchNorm2d(out_channels),
+                                nn.ReLU(inplace=True))
+    return double_conv
 
-
-class DownSample(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = DoubleConv(in_channels, out_channels)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-    def forward(self, x):
-        down = self.conv(x)
-        p = self.pool(down)
-
-        return down, p
-
-
-class UpSample(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        x = torch.cat([x1, x2], 1)
-        return self.conv(x)
-    
 
 class UNet(nn.Module):
-    def __init__(self, in_channels, num_classes):
-        super().__init__()
-        self.down_convolution_1 = DownSample(in_channels, 64)
-        self.down_convolution_2 = DownSample(64, 128)
-        self.down_convolution_3 = DownSample(128, 256)
-        self.down_convolution_4 = DownSample(256, 512)
+    def __init__(self, input_channels, num_classes):
+        super(UNet, self).__init__()
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.bottle_neck = DoubleConv(512, 1024)
+        self.down_convolution_1 = double_convolution(input_channels, 64)
+        self.down_convolution_2 = double_convolution(64, 128)
+        self.down_convolution_3 = double_convolution(128, 256)
+        self.down_convolution_4 = double_convolution(256, 512)
+        self.down_convolution_5 = double_convolution(512, 1024)
 
-        self.up_convolution_1 = UpSample(1024, 512)
-        self.up_convolution_2 = UpSample(512, 256)
-        self.up_convolution_3 = UpSample(256, 128)
-        self.up_convolution_4 = UpSample(128, 64)
+        self.up_transpose_1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up_convolution_1 = double_convolution(1024, 512)
+        self.up_transpose_2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+        self.up_convolution_2 = double_convolution(512, 256)
+        self.up_transpose_3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up_convolution_3 = double_convolution(256, 128)
+        self.up_transpose_4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.up_convolution_4 = double_convolution(128, 64)
 
         self.out = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=1)
 
+
     def forward(self, x):
-        down_1, p1 = self.down_convolution_1(x)
-        down_2, p2 = self.down_convolution_2(p1)
-        down_3, p3 = self.down_convolution_3(p2)
-        down_4, p4 = self.down_convolution_4(p3)
-
-        b = self.bottle_neck(p4)
-
-        up_1 = self.up_convolution_1(b, down_4)
-        up_2 = self.up_convolution_2(up_1, down_3)
-        up_3 = self.up_convolution_3(up_2, down_2)
-        up_4 = self.up_convolution_4(up_3, down_1)
-
-        out = self.out(up_4)
+        down_1 = self.down_convolution_1(x)
+        down_2 = self.max_pool2d(down_1)
+        down_3 = self.down_convolution_2(down_2)
+        down_4 = self.max_pool2d(down_3)
+        down_5 = self.down_convolution_3(down_4)
+        down_6 = self.max_pool2d(down_5)
+        down_7 = self.down_convolution_4(down_6)
+        down_8 = self.max_pool2d(down_7)
+        down_9 = self.down_convolution_5(down_8)
+        
+        up_1 = self.up_transpose_1(down_9)
+        x = self.up_convolution_1(torch.cat([down_7, up_1], 1))
+        up_2 = self.up_transpose_2(x)
+        x = self.up_convolution_2(torch.cat([down_5, up_2], 1))
+        up_3 = self.up_transpose_3(x)
+        x = self.up_convolution_3(torch.cat([down_3, up_3], 1))
+        up_4 = self.up_transpose_4(x)
+        x = self.up_convolution_4(torch.cat([down_1, up_4], 1))
+        out = self.out(x)
         return out
     
 
 if __name__ == '__main__':
     input_image = torch.rand((1, 3, 512, 512))
-    model = UNet(in_channels=3, num_classes=1)
+    model = UNet(input_channels=3, num_classes=10)
     total_params = sum(p.numel() for p in model.parameters())
     print(f"{total_params:,} total parameters.")
     total_trainable_params = sum(
