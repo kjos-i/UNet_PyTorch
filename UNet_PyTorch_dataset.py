ff933e5544b1e726ca1ee5fce115a596dd9d30a3
kjos-i <kjos007@gmail.com>
2025-11-09 14:52:37 +0100

    Update UNet_PyTorch_dataset.py

--- UNet_PyTorch_dataset.py
+++ UNet_PyTorch_dataset.py
@@ -3,10 +3,6 @@ import torchvision.transforms.v2 as transforms
 import os
 from PIL import Image
 
-"""
-This is a modified version of the UNet implementation by found here:
-https://medium.com/@fernandopalominocobo/mastering-u-net-a-step-by-step-guide-to-segmentation-from-scratch-with-pytorch-6a17c5916114
-"""
 
 class MyDataset(Dataset):
     def __init__(self, root_path, transform, limit=None):
@@ -14,8 +10,8 @@ class MyDataset(Dataset):
         
         self.root_path = root_path
         self.limit = limit
-        self.images = sorted([root_path + "/train_imgs/" + i for i in os.listdir(root_path + "/train_imgs/")])[:self.limit]
-        self.masks = sorted([root_path + "/train_masks/" + i for i in os.listdir(root_path + "/train_masks/")])[:self.limit]
+        self.image_list = sorted([root_path + "/train_imgs/" + i for i in os.listdir(root_path + "/train_imgs/")])[:self.limit]
+        self.mask_list = sorted([root_path + "/train_masks/" + i for i in os.listdir(root_path + "/train_masks/")])[:self.limit]
 
         if transform == "transform_augmentation":
             self.transform = transforms.Compose([
@@ -33,17 +29,18 @@ class MyDataset(Dataset):
                 ])
         
         if self.limit is None:
-            self.limit = len(self.images)
+            self.limit = len(self.image_list)
 
 
     def __getitem__(self, index):
-        img = Image.open(self.images[index]).convert("RGB")
-        mask = Image.open(self.masks[index]).convert("L")
+        img = Image.open(self.image_list[index]).convert("RGB")
+        mask = Image.open(self.mask_list[index]).convert("L")
 
         return self.transform(img, mask)
         
 
     def __len__(self):
-        return min(len(self.images), self.limit)
+        return min(len(self.image_list), self.limit)
+
 
 
